f158a98eae3d8ddd26787f22aeeb074d1ce37aab
cgtrout <cgtrout@hotmail.com>
2023-03-22 04:02:43 -0600

    Sound - Filters now use floats

--- src/CgtFilter.h
+++ src/CgtFilter.h
@@ -17,21 +17,21 @@ Class ButterworthHighPassFilter
 */
 class ButterworthHighPassFilter {
 public:
-    ButterworthHighPassFilter(double cutoff_frequency, double sample_rate)
+    ButterworthHighPassFilter(float cutoff_frequency, float sample_rate)
     : previous_input_(0), previous_output_(0) {
          // Calculate RC (resistor-capacitor) value using the cutoff frequency
-        double RC = 1.0 / (2.0 * M_PI * cutoff_frequency);
+        float RC = 1.0f / (2.0f * (float)M_PI * cutoff_frequency);
         
         // Calculate the time step (dt) using the sample rate
-        double dt = 1.0 / sample_rate;
+        float dt = 1.0f / sample_rate;
         
         // Calculate the alpha (filter coefficient) using RC and dt
         alpha_ = RC / (RC + dt);
     }
 
-    double process(double input) {
+    float process(float input) {
         // Calculate the output using the filter coefficient (alpha), previous input, and previous output
-        double output = alpha_ * previous_output_ + alpha_ * (input - previous_input_);
+        float output = alpha_ * previous_output_ + alpha_ * (input - previous_input_);
         
         previous_input_ = input;
         previous_output_ = output;
@@ -40,9 +40,9 @@ public:
     }
 
 private:
-    double alpha_;
-    double previous_input_;
-    double previous_output_;
+    float alpha_;
+    float previous_input_;
+    float previous_output_;
 };
 
 /*
@@ -54,28 +54,28 @@ Class HighPassFilter
 */
 class HighPassFilter {
 public:
-    HighPassFilter( double cutoff_frequency, double sample_rate ) : 
+    HighPassFilter( float cutoff_frequency, float sample_rate ) : 
         cutoff_frequency_( cutoff_frequency ), sample_rate_( sample_rate ),
-        RC_( 1.0 / ( cutoff_frequency_ * 2 * 3.14159265 ) ), 
-        dt_( 1.0 / sample_rate_ ),
+        RC_( 1.0f / ( cutoff_frequency_ * 2 * (float)M_PI) ), 
+        dt_( 1.0f / sample_rate_ ),
         alpha_( RC_ / ( RC_ + dt_ ) ), 
         x_previous_( 0 ), y_previous_( 0 ) {}
 
-    double process( double x_current ) {
-        double y_current = alpha_ * y_previous_ + alpha_ * ( x_current - x_previous_ );
+    float process( float x_current ) {
+        float y_current = alpha_ * y_previous_ + alpha_ * ( x_current - x_previous_ );
         x_previous_ = x_current;
         y_previous_ = y_current;
         return y_current;
     }
 
 private:
-    double cutoff_frequency_;
-    double sample_rate_;
-    double RC_;
-    double dt_;
-    double alpha_;
-    double x_previous_;
-    double y_previous_;
+    float cutoff_frequency_;
+    float sample_rate_;
+    float RC_;
+    float dt_;
+    float alpha_;
+    float x_previous_;
+    float y_previous_;
 };
 
 /*
@@ -87,22 +87,22 @@ Class ButterworthLowPassFilter
 */
 class ButterworthLowPassFilter {
 public:
-    ButterworthLowPassFilter(double cutoff_frequency, double sample_rate)
+    ButterworthLowPassFilter(float cutoff_frequency, float sample_rate)
     : previous_output_(0) {
-        double RC = 1.0 / (2.0 * M_PI * cutoff_frequency);
-        double dt = 1.0 / sample_rate;
+        float RC = 1.0f / ( 2.0f * (float)M_PI * cutoff_frequency );
+        float dt = 1.0f / sample_rate;
         alpha_ = dt / (RC + dt);
     }
 
-    double process(double input) {
-        double output = alpha_ * input + (1.0 - alpha_) * previous_output_;
+    float process(float input) {
+        float output = alpha_ * input + (1.0f - alpha_) * previous_output_;
         previous_output_ = output;
         return output;
     }
 
 private:
-    double alpha_;
-    double previous_output_;
+    float alpha_;
+    float previous_output_;
 };
 
 }//namespace
--- src/NesSound.h
+++ src/NesSound.h
@@ -51,8 +51,6 @@ namespace NesApu {
 		CgtLib::HighPassFilter highPassFilter440hz;
 		CgtLib::ButterworthLowPassFilter lowPassFilter14hz;
 
-		
-
 	  private:
 		//these are used for calculating average sample value
 		int sampleNum;
@@ -74,8 +72,7 @@ namespace NesApu {
 
 		//actual sound buffer
 		std::vector<Sint16> buffer;
-		CgtLib::CircularBuffer<Sint16> movingAverage;
-		
+		CgtLib::CircularBuffer<Sint16> movingAverage;	
 	};
 
 	/*
