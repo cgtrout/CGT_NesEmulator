41c56ce6aca0d6264ac2a24db2fc1083febda603
Corry Trout <cgtrout@hotmail.com>
2023-03-21 20:07:25 -0600

    Add cgtFilter header file
    Contains implementations of two filter types (butterworth lowpass and highpass filters)

---
+++ src/CgtFilter.h
@@ -0,0 +1,68 @@
+#pragma once
+
+//Filter Classes
+//ChatGPT generated
+//
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+namespace CgtLib {
+/*
+================================================================
+================================================================
+Class ButterworthHighPassFilter
+================================================================
+================================================================
+*/
+class ButterworthHighPassFilter {
+public:
+    ButterworthHighPassFilter(double cutoff_frequency, double sample_rate)
+    : previous_input_(0), previous_output_(0) {
+        double RC = 1.0 / (2.0 * M_PI * cutoff_frequency);
+        double dt = 1.0 / sample_rate;
+        alpha_ = RC / (RC + dt);
+    }
+
+    double process(double input) {
+        double output = alpha_ * previous_output_ + alpha_ * (input - previous_input_);
+        previous_input_ = input;
+        previous_output_ = output;
+        return output;
+    }
+
+private:
+    double alpha_;
+    double previous_input_;
+    double previous_output_;
+};
+
+
+/*
+================================================================
+================================================================
+Class ButterworthHighPassFilter
+================================================================
+================================================================
+*/
+class ButterworthLowPassFilter {
+public:
+    ButterworthLowPassFilter(double cutoff_frequency, double sample_rate)
+    : previous_output_(0) {
+        double RC = 1.0 / (2.0 * M_PI * cutoff_frequency);
+        double dt = 1.0 / sample_rate;
+        alpha_ = dt / (RC + dt);
+    }
+
+    double process(double input) {
+        double output = alpha_ * input + (1.0 - alpha_) * previous_output_;
+        previous_output_ = output;
+        return output;
+    }
+
+private:
+    double alpha_;
+    double previous_output_;
+};
+
+}//namespace
