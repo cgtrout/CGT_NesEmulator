770c09e03de77afa6c22abc8f97a6ef1338940b0
Corry Trout <cgtrout@hotmail.com>
2023-03-25 20:29:30 -0600

    Mapper1 - start implementation

--- src/NesMappers.cpp
+++ src/NesMappers.cpp
@@ -28,22 +28,39 @@ void NesMapper0::reset() {
 /*
 =============================================================================
 Mapper 1 - MMC1
+
+https://www.nesdev.org/wiki/MMC1
+
+
+- Change register value by writing five times with bit 7 clear and desired value in bit 0 
+  (starting with low bit)
+- First four writes: MMC1 shifts bit 0 into shift register
+- Fifth write: MMC1 copies bit 0 and shift register contents into internal register (selected 
+  by bits 14 and 13 of address), then clears shift register
+- Only fifth write's address matters, specifically bits 14 and 13
+- After fifth write, shift register is cleared automatically, no need to write again with 
+  bit 7 set
 =============================================================================
 */
 class FuncObjMapper1 : public FunctionObjectBase {
 public:
 	void write( uword address, ubyte param ) {
-		/*
-		systemMain->nesMain.nesMemory.fillPrgBanks(
-			0x8000,						//start pos
-			param * PRG_BANKS_PER_PAGE,	//prg rom page
-			0x4000 / CPU_BANKSIZE );	//size of transfer
-			*/
+		//Writing a value with bit 7 set( $80 through $FF ) to any address in 
+		//$8000 - $FFFF clears the shift register to its initial state
+		if( BIT( 7, param ) ) {
+			MMC1_SR = 0b10000;
+		}
+		else {
+			MMC1_SR = MMC1_SR >> 1;
+		}
 	}
 
 	ubyte read( uword address ) {
 		return 0;
 	}
+
+	ubyte MMC1_SR = 0b10000;	//shift register
+	ubyte MMC1_PB = 0;	//program bank selection
 
 
 } funcObjMapper1;
