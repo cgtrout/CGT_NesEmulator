80466d2c974608762bd79f469533aaa32cbeaf21
Corry Trout <cgtrout@hotmail.com>
2023-03-30 08:44:19 -0600

    NesMappers - remove comments

--- src/NesMappers.cpp
+++ src/NesMappers.cpp
@@ -134,13 +134,7 @@ void NesMapper1::initializeMap( ) {
 					else if( address >= 0xA000 && address <= 0xBFFF ) {
 						ubyte selected_bank = MMC1_PB & 0b11111;
 
-						//todo may need to mask here?
-						//Total ppu is 8kb
 						switch( char_rom_switch_size ) {
-						case 4:
-							//TODO sets bank for PPU 0000-0FFF (4kb region)
-							// full 5 bits are used - 32 possible banks
-							break;
 						case 8:
 							//bottom bit is ignored (nerdy nights) so there are 16 possible banks
 							selected_bank &= 0b11110;
